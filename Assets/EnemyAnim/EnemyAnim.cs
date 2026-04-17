767edb625c01b9a0ec377d99fc7d4db8dc3ec092
dariaivanovna05-create <dariaivanovna05@gmail.com>
2026-04-17 19:25:29 +0200

    animacion_npcs

--- Assets/EnemyAnim/EnemyAnim.cs
+++ Assets/EnemyAnim/EnemyAnim.cs
@@ -1,14 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAnim : MonoBehaviour
 {
+    Animator anim;
+
+    NavMeshAgent navMeshA;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        navMeshA = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        
+        if(navMeshA.isOnOffMeshLink)
+        {
+            anim.SetInteger("jump", 1);
+        }
+
+        else
+        {
+            anim.SetInteger("jump", 0);
+        }
     }
+
+    /*void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Salta"))
+        {
+            Debug.Log("Salta");
+            anim.SetInteger("jump", 1);
+        }
+    }*/
 }
