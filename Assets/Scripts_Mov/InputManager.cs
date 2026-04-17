b56410f66be572f89a2aa3b38e730721171e7dc9
dariaivanovna05-create <dariaivanovna05@gmail.com>
2026-04-17 16:38:54 +0200

    boing boing

--- Assets/Scripts_Mov/InputManager.cs
+++ Assets/Scripts_Mov/InputManager.cs
@@ -4,8 +4,14 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public Vector2 movementInput;
+    public bool jumpPressed;
     public void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        jumpPressed = value.isPressed;
     }
 }
--- Assets/Scripts_Mov/PlayerMovement.cs
+++ Assets/Scripts_Mov/PlayerMovement.cs
@@ -1,20 +1,37 @@
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 2f;
     private InputManager inputManager;
+    private Rigidbody rb;
+    private bool isGrounded = true;
 
     void Awake()
     {
         inputManager = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         Vector3 move = new Vector3(inputManager.movementInput.x, 0, inputManager.movementInput.y);
-
         transform.position += move * speed * Time.deltaTime;
+
+        if (inputManager.jumpPressed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isGrounded = false;
+            Debug.Log("salta");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isGrounded = true;
+        }
     }
 }
